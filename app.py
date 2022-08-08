3766f02c4b0c831b1a576a27716df9690f792003
Phill Legnitto <palegnitto@gmail.com>
2022-08-08 12:09:05 -0700

    created Cupcake Model

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, render_template, flash
+
+
+from models import db, connect_db, Playlist, Song, PlaylistSong
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///playlist_app'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
+
--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    # TODO: check that it is ok to use string here or if tbhey actually want text
+    __tablename__ = "cupcakes"
+
+    id = db.Column(db.Integer,
+                    primary_key = True,
+                    autoincrement = True)
+
+    flavor = db.Column(db.String(25),
+                    nullable = False)
+
+    size = db.Column(db.String(25),
+                            nullable = False)
+
+    rating = db.Column(db.Integer,
+                            nullable = False)
+
+    image = db.Column(db.Text,
+                            nullable = False,
+                            default = "https://tinyurl.com/demo-cupcake")
